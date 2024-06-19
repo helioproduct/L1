71a670e9ec69ce6b21330c37224186ed9ae4bcb4
Nikolay Popov <helioproduct.sync@gmail.com>
2024-06-19 19:16:07 +0300

    03 added

---
+++ 03/main.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"fmt"
+	"sync"
+	"sync/atomic"
+)
+
+func main() {
+	var wg sync.WaitGroup
+	var sum atomic.Uint64
+
+	sourceArray := []uint64{2, 4, 6, 8, 10}
+
+	for _, number := range sourceArray {
+		wg.Add(1)
+		go func(wg *sync.WaitGroup, delta uint64) {
+			sum.Add(delta)
+			wg.Done()
+		}(&wg, number*number)
+	}
+	wg.Wait()
+
+	fmt.Println(sum.Load())
+}
