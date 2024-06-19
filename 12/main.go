5e833854b316c48066951108ffe8ad2144194880
Nikolay Popov <helioproduct.sync@gmail.com>
2024-06-19 19:17:09 +0300

    12 added

---
+++ 12/main.go
@@ -0,0 +1,36 @@
+package main
+
+import "fmt"
+
+type Set map[interface{}]struct{}
+
+func (s Set) Add(key interface{}) {
+	s[key] = struct{}{}
+}
+
+// interface{} why not
+func (s Set) AddAll(sourceArray []string) {
+	for _, str := range sourceArray {
+		s.Add(str)
+	}
+}
+
+func (s Set) Remove(key interface{}) {
+	delete(s, key)
+}
+
+func (s Set) Has(key interface{}) bool {
+	_, ok := s[key]
+	return ok
+}
+
+func main() {
+
+	sourceArray := []string{"cat", "cat", "dog", "cat", "tree"}
+	set := make(Set)
+	set.AddAll(sourceArray)
+
+	for key, _ := range set {
+		fmt.Println(key)
+	}
+}
